20ad8b528b0f5a32e2613145335d74d9cc8a34f2
Chitransh-Thakur <chitransht2000@gmail.com>
2023-06-28 18:36:09 +0530

    pushed repo

---
+++ DotNetWebApi/DotNetWebApi/Controllers/EmployeesController.cs
@@ -0,0 +1,78 @@
+﻿using DotNetWebApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public EmployeesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllEmployees()
+        { 
+          var list = _context.Employees.ToList();
+          return Ok(list);
+        }
+
+        [HttpPost]
+        public IActionResult AddEmployee([FromBody] Employee request)
+        {
+            request.Id = Guid.NewGuid();
+            _context.Employees.Add(request);
+            _context.SaveChanges();
+            return Ok();
+        }
+
+        [Route("{id:Guid}")]
+        [HttpGet]
+        public IActionResult GetEmployee([FromRoute]Guid? id)
+        {
+            var employee = _context.Employees.FirstOrDefault(x => x.Id == id);
+            if(employee == null) 
+            {
+               return NotFound();
+            }
+            return Ok(employee);
+        }
+
+        [HttpPut]
+        [Route("{id:Guid}")] 
+        public IActionResult UpdateEmployee([FromRoute]Guid? id, Employee updateRequestModel)
+        {
+            var employee = _context.Employees.Find(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            employee.Name = updateRequestModel.Name;
+            employee.Email = updateRequestModel.Email;
+            employee.Salary = updateRequestModel.Salary;
+            employee.Phone = updateRequestModel.Phone;
+            employee.Department = updateRequestModel.Department;
+            _context.Employees.Update(employee);
+            _context.SaveChanges(); 
+            return Ok();
+        }
+
+        [HttpDelete]
+        [Route("{id:Guid}")]
+        public IActionResult DeleteEmployee([FromRoute] Guid? id)
+        {
+            var employee = _context.Employees.Find(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            _context.Employees.Remove(employee);
+            _context.SaveChanges();
+            return Ok();
+        }
+    }
+}
---
+++ DotNetWebApi/DotNetWebApi/Migrations/20230620083208_IntialiseDB.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DotNetWebApi.Migrations
+{
+    public partial class IntialiseDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Phone = table.Column<long>(type: "bigint", nullable: true),
+                    Salary = table.Column<long>(type: "bigint", nullable: true),
+                    Department = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
